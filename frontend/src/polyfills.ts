05bc8bd9e6180c439acabd3238ebb082d4b746c1
uldahlalex <alex@uldahl.dk>
2022-01-31 18:14:54 +0100

    .env -> compose -> frontend dockerfile -> angular args

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -58,7 +58,7 @@ import './zone-flags';
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'angular-server-side-configuration/process';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
