9a6c8a8fb3197bc03ff61e2edce257ae6ef34249
uldahlalex <alex@uldahl.dk>
2022-02-11 23:58:06 +0100

    connection from taskservice to postgres w/ container ip

--- taskService/index.ts
+++ taskService/index.ts
@@ -2,14 +2,18 @@ import { PrismaClient } from '@prisma/client'
 import express from 'express';
 import http from 'http';
 import minimist from 'minimist';
+import axios from 'axios';
 
 const app = express();
 const server = http.createServer(app)
-//const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 const { Kafka, logLevel } = require('kafkajs')
 const argv = minimist(process.argv.slice(1));
 const port = argv['port'] || 3001;
-
+prisma.$connect().then(r => {
+    console.log(r);
+    console.log('connected');
+})
 const kafkaProducer = new Kafka({
     logLevel: logLevel.ERROR,
     brokers: ['localhost:9092'],
@@ -21,32 +25,19 @@ async function kafkaInit() {
 
 }
 
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
-
-@Entity()
-export class Task {
-
-    @PrimaryGeneratedColumn()
-    id: number;
-
-    @Column()
-    description: string;
-
-
-}
 
 kafkaInit().then(r => {
     console.log(r)
     console.log('Initialized Kafka connection')
 })
 
-app.get('/tasks', async (req, res) => {
-    //const tasks = await prisma.task.findMany()
-    res.send();
+app.get('/tasks', async (req, result) => {
+
+
 })
 
 app.post('/tasks', async(req, res) => {
-    /*await prisma.task.create({
+    await prisma.task.create({
         data: {
             title: "do something",
             desc: "now",
@@ -54,17 +45,17 @@ app.post('/tasks', async(req, res) => {
         }
     }).then(result => {
         res.send(result);
-    })*/
+    })
 
 })
 
 app.get('/sendToIdentity', async (req, res) => {
-    await kafkaProducer.send({
+    /*await kafkaProducer.send({
         topic: 'test-topic',
         messages: [
             {value: 'Hello KafkaJS user!'},
         ],
-    })
+    })*/
     res.send('sent')
 })
 
