d4a8b57455c5d4dd01bbcbc4a50f65470966886c
uldahlalex <alex@uldahl.dk>
2022-02-21 11:46:58 +0100

    chai, mocha, readme for tests

--- tests/e2e/identity-api.spec.js
+++ tests/e2e/identity-api.spec.js
@@ -1,2 +1,11 @@
-const request = require('supertest');
-const assert = require('assert');
+const request = require("supertest")("https://airportgap.dev-tester.com/api");
+const expect = require("chai").expect;
+
+describe("GET /airports", function () {
+    it("returns all airports, limited to 30 per page", async function () {
+        const response = await request.get("/airports");
+
+        expect(response.status).to.eql(200);
+        expect(response.body.data.length).to.eql(30);
+    });
+});
