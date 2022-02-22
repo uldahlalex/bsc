0d30f0f67ab5eb772b011a900c6ecfe36a3384de
uldahlalex <alex@uldahl.dk>
2022-02-22 13:18:18 +0100

    addition to identity api test file

--- tests/e2e/identity-api.spec.js
+++ tests/e2e/identity-api.spec.js
@@ -1,11 +1,31 @@
-const request = require("supertest")("https://airportgap.dev-tester.com/api");
+const request = require("supertest");
+const app = "http://localhost:3002";
 const expect = require("chai").expect;
 
-describe("GET /airports", function () {
-    it("returns all airports, limited to 30 per page", async function () {
-        const response = await request.get("/airports");
-
+describe("GET /openEndpoint", function () {
+    it("Returns 200 if connection to unauthorized endpoint is successful", async function () {
+        const response = await request(app)
+            .get("/openEndpoint");
         expect(response.status).to.eql(200);
-        expect(response.body.data.length).to.eql(30);
+
     });
 });
+
+describe("POST /login", function (done) {
+    it("Returns 201 if successfully logged in", async function () {
+        const response = await request(app)
+            .post("/login")
+            .send({email: 'alex@uldahl.dk', password: "1234"})
+        expect(response.status).to.eql(201).then(done)
+    });
+});
+/*
+describe("POST /register", function () {
+    it("Returns 201 if created new user", async function () {
+        const response = await request(app)
+            .post("/register")
+            .send({email: 'bob@uldahl.dk', password: "1234"})
+        expect(response.status).to.eql(201);
+    });
+});
+*/
