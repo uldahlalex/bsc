802d63f46e1d698566ef1d8b41664965cc3565a6
uldahlalex <alex@uldahl.dk>
2022-02-25 14:36:59 +0100

    queries and seeder neo4j methods

--- dbutils/seeder.js
+++ dbutils/seeder.js
@@ -1,71 +1,66 @@
 /**
- * Script to seed taskstore
+ * Script to seed TaskStore (Neo4j)
  */
+/*
+const {Connection} = require('cypher-query-builder');
+const {faker} = require('@faker-js/faker');
+const cypher = require('cypher-query-builder');
 
-const {faker} = require("@faker-js/faker");
-const MongoClient = require("mongodb").MongoClient;
-const mongoose = require('mongoose');
-function randomIntFromInterval(min, max) { // min and max included
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-async function seedDB() {
+const db = new Connection('bolt://localhost', {
+    username: 'neo4j',
+    password: 'test'
+})
 
-    const client = new MongoClient('mongodb://alex:q1w2e3r4@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false', {
-        useNewUrlParser: true,
-        // useUnifiedTopology: true,
-    });
-    try {
-        await client.connect();
-        console.log("Connected correctly to server");
-        const collection = client.db("test").collection("tasks");
-        // The drop() command destroys all data from a collection.
-        // Make sure you run it against proper database and collection.
-        await collection.drop();
-        // make a bunch of time series data
-        let tasks = [];
-        for (let i = 0; i < 10; i++) {
-            const title = faker.git.commitMessage();
-            const desc = faker.lorem.sentence()
-            const timestamp = faker.time.recent();
-            const subtasks = [];
-                for (let j = 0; j < 2; j++) {
-                    const id = mongoose.Types.ObjectId();
-                    const title = faker.git.commitMessage();
-                    const desc = faker.lorem.sentence()
-                    const timestamp = faker.time.recent();
-                    const innerTasks = [];
-                    for (let k = 0; k < 2; k++) {
-                        const id = mongoose.Types.ObjectId();
-                        const title = faker.git.commitMessage();
-                        const desc = faker.lorem.sentence()
-                        const timestamp = faker.time.recent();
-                        innerTasks.push({
-                            id: id,
-                            title: title,
-                            desc: desc,
-                            timestamp: timestamp
-                        })
-                    }
-                    subtasks.push({
-                        id: id,
-                        title: title,
-                        desc: desc,
-                        timestamp: timestamp,
-                        subtasks: innerTasks
-                    });
-                }
-            tasks.push({
-                title: title,
-                desc: desc,
-                timestamp: timestamp,
-                subtasks: subtasks
-            });
-        }
-        await collection.insertMany(tasks);
-        console.log("Database seeded! :)");
-        await client.close();
-    } catch (err) {
-        console.log(err.stack);
-    }
+function createOneTaskNode() {
+    db.createNode('task', 'Task', {
+        title: faker.git.commitMessage(),
+        description: faker.lorem.sentence(),
+        authorId: faker.random.alphaNumeric(),
+    })
+        .run()
+        .then(r => console.log(r));
 }
-seedDB();
+
+function relateNodeToOtherNode() {
+    db.matchNode('task', 'Task', {identity: 0})
+        .create(cypher.relation('out', 'rel','SUBTASK'))
+        .matchNode('task', 'Task', {identity: 1})
+        .run();
+}
+
+relateNodeToOtherNode();
+*/
+
+const neo4j = require('neo4j-driver');
+const driver = neo4j.driver('neo4j://localhost', neo4j.auth.basic('neo4j', 'test'));
+const session = driver.session();
+const {faker} = require('@faker-js/faker');
+
+function createOneTaskNode() {
+    session.run('' +
+        'CREATE (task:Task {' +
+        'title: $title, ' +
+        'description: $description,' +
+        'authorId: $authorId }) RETURN task',
+        {
+            title: faker.git.commitMessage(),
+            description: faker.lorem.sentence(),
+            authorId: faker.random.alphaNumeric(),
+        }).then(result => {
+            console.log(result)
+    })
+}
+
+function createRelationship() {
+    session.run('' +
+        'MATCH (a:Task), (b:Task)\n' +
+        'WHERE ID(a) = $aIdentity AND ID(b) = $bIdentity\n' +
+        'CREATE (a)-[:SUBTASK]->(b);',
+        {
+            aIdentity: 2,
+            bIdentity: 0
+        }).then(result => {
+          console.log(result);
+        })
+}
+
