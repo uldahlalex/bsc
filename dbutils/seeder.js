0d5db17eca36c01d8310401872f08c0fb5257995
uldahlalex <alex@uldahl.dk>
2022-03-03 20:15:38 +0100

    new seeding functions

--- dbutils/seeder.js
+++ dbutils/seeder.js
@@ -67,5 +67,49 @@ function seed() {
         })
     }
 }
-seed()
 
+//52 - 61
+async function createOrganizations() {
+    for(let i = 0; i<10; i++){
+        let session = driver.session();
+        session.run('' +
+            'CREATE (o: Organization {name: $name});',
+            {
+                name: 'Organization_'+i
+            }).then(res => {
+                session.close();
+        })
+    }
+}
+
+async function createProjects() {
+    for (let i = 52; i<62; i++) {
+        for (let k = 0; k < 10; k++) {
+            let session = driver.session();
+            session.run('' +
+                'MATCH (o:Organization) WHERE ID(o)=$orgId \n' +
+                'CREATE (p:Project {name: $name})<-[:CHILDREN]-(o)',
+                {
+                    orgId: i,
+                    name: "Project_"+k
+                }).then(res => {
+                    console.log(res);
+                session.close();
+            })
+        }
+    }
+}
+
+async function createTasks() {
+    for(let i = 112; i<209; i++) {
+        for(let k = 0; k<10; k++) {
+            let session= driver.session();
+            session.run(
+            'MATCH (p:Task) WHERE ID(p)=$projectId \n' +
+            'CREATE (t:Task {name: $name})<-[:CHILDREN]-(p)', {
+                projectId: i,
+                    name: "Task_"+k
+                })
+        }
+    }
+}
