6b9af260b3322972331c1855010025c5e2660794
uldahlalex <41024316+uldahlalex@users.noreply.github.com>
2022-03-30 17:02:44 +0200

    seeder function for cassandra

--- activityService/infrastructure/infrastructure.shared.ts
+++ activityService/infrastructure/infrastructure.shared.ts
@@ -2,6 +2,18 @@ import cassandra from "cassandra-driver";
 import minimist from 'minimist';
 const argv = minimist(process.argv.slice(1));
 
+var seeder: cassandra.Client = new cassandra.Client({
+    contactPoints: ['localhost:9042']
+})
+
+seeder.connect(function(e) {
+    var query = "create keyspace IF NOT EXISTS mykeyspace with replication = {'class': 'org.apache.cassandra.locator.NetworkTopologyStrategy', 'datacenter1': '3'};";
+    return client.execute(query, function(e, res) {
+        return console.log(e, res);
+    });
+});
+
+
 var client;
 
 if(argv['client']==undefined || argv['pass']==undefined) {
@@ -10,7 +22,6 @@ if(argv['client']==undefined || argv['pass']==undefined) {
         localDataCenter: 'datacenter1',
         keyspace: 'mykeyspace'
     });
-    //Possibly create new schema here for local development
 } else {
     client = new cassandra.Client({
         cloud: {
@@ -23,6 +34,20 @@ if(argv['client']==undefined || argv['pass']==undefined) {
     }, );
 }
 
-client.connect().then(r => console.log('Connected to Cassandra'));
+client.connect(function(e) {
+    let table = "CREATE TABLE IF NOT EXISTS Actions (\n" +
+        "   userId text,\n" +
+        "   actionType text,\n" +
+        "   endpoint text,\n" +
+        "   service text,\n" +
+        "   bodyItems text,\n" +
+        "   organizationId bigint,\n" +
+        "   eventTime timestamp,\n" +
+        "   PRIMARY KEY(userId, eventTime)\n" +
+        ") WITH CLUSTERING ORDER BY (eventtime DESC);\n"
+    let organizationIndex = "CREATE INDEX IF NOT EXISTS organizationid ON mykeyspace.actions (organizationid);";
+    client.execute(table);
+    client.execute(organizationIndex);
+})
 
 export const cassandraClient = client;
